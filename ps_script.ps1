0bc936a4324890ea2d723de143eb4bf4afe36d85
NomanNasirMinhas <sp17bsse010@gmail.com>
2026-03-26 11:31:13 +0300

    init

--- ps_script.ps1
+++ ps_script.ps1
@@ -1,9 +1,13 @@
-
 $user = [System.Security.Principal.WindowsIdentity]::GetCurrent()
-# Get the current user's groups
-$groups = $user.Groups | For Each-Object { $_.Translate([System.Security.Principal.NTAccount]) } 
-# Get the current user's privileges
-$privileges = [System.Security.Principal.WindowsIdentity]::GetCurrent().Groups | For Each-Object { $_.Translate([System.Security.Principal.NTAccount]) }
-# Output the user information, groups, and privileges in a  text file in the public folder
+$groups = $user.Groups | ForEach-Object { 
+    try { 
+        $_.Translate([System.Security.Principal.NTAccount]).Value 
+    } catch { 
+        $_.Value
+    } 
+}
+
+$privileges = (whoami /priv /fo csv | ConvertFrom-Csv) | Select-Object -ExpandProperty "Privilege Name"
+
 $output = "User: $($user.Name)`nGroups: $($groups -join ', ')`nPrivileges: $($privileges -join ', ')"
 $output | Out-File -FilePath "C:\Users\Public\whoami.txt"
